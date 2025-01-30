1a8e2e84706c3727ff64e51c8277f68e8f66f22a
Aviad Korakin <aviad825@gmail.com>
2025-01-30 21:50:39 +0200

    init

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,6 +2,8 @@ import User from "@/models/User";
 import {connectToDatabase} from "@/lib/mongoose";
 
 export async function findOrCreateUser(github_id: string) {
+    if(!github_id) return undefined;
+    
     await connectToDatabase();
 
     let user = await User.findOne({ github_id: github_id });
