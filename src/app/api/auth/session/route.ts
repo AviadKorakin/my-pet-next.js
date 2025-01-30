177ef9c3eea64b08a3eadc16eb98e92243c32643
Aviad Korakin <aviad825@gmail.com>
2025-01-30 22:25:47 +0200

    init

--- src/app/api/auth/session/route.ts
+++ src/app/api/auth/session/route.ts
@@ -3,21 +3,9 @@ import { NextResponse } from "next/server";
 import {authOptions} from "@/lib/auth";
 
 export async function GET(req: Request) {
-    console.log("🔹 [LOG] Session API Called"); // ✅ Log when the API is hit
-
-    // Log incoming cookies
-    const cookies = req.headers.get("cookie");
-    console.log("🔹 Incoming Cookies:", cookies); // ✅ Log all cookies
-
-
     const session = await getServerSession(authOptions);
-
-    console.log("🔹 [LOG] Session Data:", session); // ✅ Log the session data
-
     if (!session) {
-        console.log("❌ [ERROR] No Session Found - Returning 401");
         return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
-
     return NextResponse.json({ user: session.user });
 }
